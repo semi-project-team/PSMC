847ed43a0b6a3654d1ba0cc1391029df1770782b
wjdals83 <tjwjdals0803@naver.com>
2024-05-13 15:27:38 +0900

    mediConnects 중간푸시

---
+++ project/PSMC/src/main/java/com/javaclass/psmc/mediConnect/controller/MediConnectController.java
@@ -0,0 +1,44 @@
+package com.javaclass.psmc.mediConnect.controller;
+
+import com.javaclass.psmc.mediConnect.model.dto.ShowMediConnectDTO;
+import com.javaclass.psmc.mediConnect.model.service.MediConnectService;
+import com.javaclass.psmc.user.model.dto.LoginUserDTO;
+import jakarta.servlet.http.HttpSession;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class MediConnectController {
+
+    private final MediConnectService mediConnectService;
+
+    @Autowired
+    public MediConnectController(MediConnectService mediConnectService) {
+        this.mediConnectService = mediConnectService;
+    }
+
+    @GetMapping("/doc/mediConnect/{projectNo}")
+    public String mediConnectPage(@PathVariable int projectNo, Model model, HttpSession session) {
+
+        LoginUserDTO loginUserDTO = (LoginUserDTO) session.getAttribute("auth");
+        String pmCode = loginUserDTO.getPmCode();
+
+        Map<String,Object> parameter = new HashMap<>();
+        parameter.put("pmCode",pmCode);
+        parameter.put("projectNo",projectNo);
+
+        List<ShowMediConnectDTO> boards = mediConnectService.showAllBoards(parameter);
+
+        model.addAttribute("boards", boards);
+
+        return "/doc/mediConnect";
+    }
+}
