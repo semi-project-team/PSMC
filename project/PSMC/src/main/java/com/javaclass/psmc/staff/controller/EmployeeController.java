f600b1356c56cba13f47a3cd56f2e158297afc6a
jaejae <santoki389@gmail.com>
2024-05-28 11:58:47 +0900

    [Feature] alert 기능 추가

---
+++ project/PSMC/src/main/java/com/javaclass/psmc/staff/controller/EmployeeController.java
@@ -0,0 +1,61 @@
+package com.javaclass.psmc.staff.controller;
+
+import com.javaclass.psmc.common.model.dto.MedicalFieldDTO;
+import com.javaclass.psmc.staff.model.dto.EmployeeFieldPositionDTO;
+import com.javaclass.psmc.staff.model.dto.EmployeeToMedicalFieldDTO;
+import com.javaclass.psmc.staff.model.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/staff/list")
+    public String findEmployeeList(Model model){
+
+        List<EmployeeFieldPositionDTO> employeeList = employeeService.findAllEmployee();
+
+        for(EmployeeFieldPositionDTO employeeDTO : employeeList){
+            System.out.println("employeeDTO = " + employeeDTO)                                                                                                                                                  ;
+        }
+
+        List<MedicalFieldDTO> allField = employeeService.findAllField();
+
+        model.addAttribute("medicalField",allField);
+        model.addAttribute("employeeList", employeeList);
+
+        return "/staff/list";
+    }
+
+    @GetMapping("/search")
+    public String searchByParam(@RequestParam Map<String,String> parameter, Model model) {
+
+        System.out.println("parameter = " + parameter);
+        List<EmployeeToMedicalFieldDTO> employeeList= employeeService.findMemberByFieldCode(parameter);
+        if(employeeList.size() == 0) {
+            model.addAttribute("isNull", true);
+        }
+        model.addAttribute("employeeList",employeeList);
+        System.out.println("employeeList = " + employeeList);
+
+        for(EmployeeToMedicalFieldDTO e: employeeList){
+            System.out.println("e = " + e);
+        }
+
+        return "/staff/search";
+    }
+
+}
